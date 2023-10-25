44047837652d59ead21fb27c322433cd7fc2c72c
Quentin Morineau <q.morineau@gmail.com>
2023-10-25 12:27:24 +0200

    commit search func

--- app.js
+++ app.js
@@ -33,17 +33,24 @@ var swiper = new Swiper(".mySwiper", {
 
 // Variables
 
-let popup = document.querySelector(".popup");
-let crossDiv = document.querySelector(".cross");
-let cross = crossDiv.querySelector("img");
-let searchingBar = document.querySelector("#search")
 const options = {
   method: 'GET',
   headers: {
     accept: 'application/json',
     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMTJjZjE0ZDQzNzQyZjJiMWE1ZWFjMmNkMmQxMDU3MiIsInN1YiI6IjY1MzhjMDEzYWUzNjY4MDBhZGE4MDIzNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.L1jWmZ_mygneYzG_Jd0RZ57Avi5K1qyiSvvFXwl6ybc'
   }
-};
+}
+let popup = document.querySelector(".popup");
+let crossDiv = document.querySelector(".cross");
+let cross = crossDiv.querySelector("img");
+let searchingBar = document.querySelector("#search");
+let mySwiper1 = document.querySelector(".mySwiper");
+let swiper1 = mySwiper1.querySelector(".swiper-wrapper");
+let mySwiper2 = document.querySelector(".mySwiper1");
+let swiper2 = mySwiper2.querySelector(".swiper-wrapper");
+let mySwiper3 = document.querySelector(".mySwiper2");
+let swiper3 = mySwiper3.querySelector(".swiper-wrapper");
+
 
 // Functions
 
@@ -64,7 +71,13 @@ let fetchBySearch = async (search) => {
     let data = await res.json()
     let arrayData = data.results
     arrayData.forEach(element => {
-      posterFunc(element.poster_path)
+      let newDiv = document.createElement("div");
+      let swiperSlide = swiper1.appendChild(newDiv);
+      swiperSlide.classList.add("swiper-slide");
+      let newImg = document.createElement("img");
+      let img = swiperSlide.appendChild(newImg);
+      img.src = posterFunc(element.poster_path);
+      /* posterFunc(element.poster_path) */
     });
     console.log(arrayData)
   } catch (error) {
@@ -73,27 +86,26 @@ let fetchBySearch = async (search) => {
 }
 
 let posterFunc = (url) => {
-  console.log(`https://image.tmdb.org/t/p/original${url}`)
+  return `https://image.tmdb.org/t/p/original${url}`
+}
+// Reset Swiper
+
+let resetSwiper = (swiper) => {
+  swiper.innerHTML = ""
 }
 
-/* let fetchLatest = async () => {
-  try {
-    let res = await fetch('https://api.themoviedb.org/3/movie/latest', options)
-    let data = await res.json()
-    console.log(data)
-  } catch (error) {
-      console.log(error)
-  }
-} */
+let resetAllSwiper = () => {
+  resetSwiper(swiper1)
+  resetSwiper(swiper2)
+  resetSwiper(swiper3)
+}
 
-let loginCheck = () => {
+/* let loginCheck = () => {
   let username = document.querySelector("#username");
   let password = document.querySelector("#password");
   let rememberCheckbox = document.querySelector("#remember");
   let menu = document.querySelector(".menu");
-  /* let openSignIn = menu.childElement[5].children; */
-  
-  
+  // let openSignIn = menu.childElement[5].children;
   
   if (username.value.trim()) {
     let info = {
@@ -106,10 +118,10 @@ let loginCheck = () => {
     password.value = "";
     rememberCheckbox.checked = false;
   }
-}
+} */
 
 // Actions
-/* fetchBySearch("pirate") */
+resetAllSwiper()
 document.addEventListener("click", (e) => {
   if (e.target === cross) {
     popup.style.display = "none"
