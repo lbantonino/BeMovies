3ca0d1587ec5a21ff6a78c3735c73daf37650372
Antonino Lo Bianco <antonino.lobianco@outlook.com>
2023-11-07 09:40:01 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,37 @@
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 4,
     spaceBetween: 19,
+    mousewheel: true,
+      loop: true,
     navigation: {
         nextEl: ".button-next",
         prevEl: ".button-prev",
         clickable: true,
       },
-      mousewheel: true,
   });
 
   var newSwiper = new Swiper(".mySwiper1", {
     slidesPerView: 4,
     spaceBetween: 19,
+    mousewheel: true,
+    loop: true,
     navigation: {
         nextEl: ".button-next1",
         prevEl: ".button-prev1",
         clickable: true,
       },
-      mousewheel: true,
   });
 
   var newSwiper1 = new Swiper(".mySwiper2", {
     slidesPerView: 4,
     spaceBetween: 19,
+    mousewheel: true,
+    loop: true,
     navigation: {
         nextEl: ".button-next2",
         prevEl: ".button-prev2",
         clickable: true,
       },
-      mousewheel: true,
   });
 
 // Variables
